c4c16a8bd5200a05154f8ec860128eef10234eae
Alice <alisahalitova0898@gmail.com>
2024-06-14 15:51:19 +0500

    runner2

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def appointment(psycholog, patient, date):
 async def psycholog_name(name):
     str = 'select find_psycholog_by_name(\'' + name + '\')'
     cursor.execute(str)
-    a = 3
+    a = 4
     records = cursor.fetchone()
     return records
 
